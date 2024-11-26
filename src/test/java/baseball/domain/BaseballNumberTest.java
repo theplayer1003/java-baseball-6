f4cf899bc3aba0d5e1cea481ed9b2eb706bfa6f1
theplayer1003 <theplayer1003@gmail.com>
2024-11-26 10:49:09 +0900

    test: BaseballNumber 클래스 테스트 작성

---
+++ src/test/java/baseball/domain/BaseballNumberTest.java
@@ -0,0 +1,34 @@
+package baseball.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BaseballNumberTest {
+    private BaseballNumber number1;
+    private BaseballNumber number3;
+
+    @BeforeEach
+    void init() {
+        number1 = new BaseballNumber(1);
+        number3 = new BaseballNumber(3);
+    }
+
+    @Test
+    void test_equals() {
+        BaseballNumber numberForEqualTest = new BaseballNumber(1);
+
+        assertEquals(number1, numberForEqualTest);
+        assertNotEquals(number3, numberForEqualTest);
+    }
+
+    @Test
+    void validate_check() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            new BaseballNumber(10);
+        });
+
+        assertEquals("숫자는 1부터 9까지의 정수만 사용합니다.", exception.getMessage());
+    }
+}
